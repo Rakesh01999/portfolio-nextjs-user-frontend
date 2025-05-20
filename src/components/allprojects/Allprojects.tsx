2059dafc96910581437b24a28557a4dae061a295
Rakesh Biswas <rbiswas01999@gmail.com>
2025-05-20 14:25:42 +0600

    All Projects updated

--- src/components/allprojects/Allprojects.tsx
+++ src/components/allprojects/Allprojects.tsx
@@ -42,15 +42,17 @@ const AllProject = () => {
   return (
     <div className="section projects-section mx-4 md:mx-10 lg:mx-14 mb-20">
       <Fade>
-        
-        <h2 className="hover:text-blue-600 text-xl text-center md:text-[40px] font-bold mb-10">
+        <h2 className=" text-xl text-center md:text-[40px] font-bold mb-10">
+          All Projects
+        </h2>
+        <h2 className="text-cyan-700 text-xl text-center md:text-[40px] font-bold mt-20 mb-10">
           Projects
         </h2>
       </Fade>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20">
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20">
         {project?.map((proj) => (
           <Zoom key={proj._id}>
-            <div className="skill-category bg-base-100 border-2 border-blue-400 hover:border-cyan-300 p-6 rounded-lg shadow-lg hover:shadow-2xl hover:shadow-cyan-200 transition-shadow duration-300">
+            <div className="skill-category bg-base-100 border-2 border-gray-400 hover:border-cyan-400 p-6 rounded-lg shadow-lg hover:shadow-2xl hover:shadow-cyan-200 transition-shadow duration-300">
               <div className="project-image mb-4">
                 <Image
                   src={proj.image}
@@ -71,7 +73,7 @@ const AllProject = () => {
               <div className="text-center flex justify-center space-x-4 mt-4">
                 <Link
                   href={proj.project_link}
-                  className="text-blue-500 hover:text-blue-700"
+                  className="text-cyan-600 hover:text-gray-700"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -79,7 +81,7 @@ const AllProject = () => {
                 </Link>
                 <Link
                   href={proj.github_link}
-                  className="text-blue-500 hover:text-blue-700"
+                  className="text-cyan-600 hover:text-gray-700"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -87,7 +89,7 @@ const AllProject = () => {
                 </Link>
                 <Link
                   href={`${`projects/${proj._id}`}`}
-                  className="text-blue-500 hover:text-blue-700"
+                  className="text-cyan-600 hover:text-gray-700 md:text-xl"
                 >
                   Description
                 </Link>
